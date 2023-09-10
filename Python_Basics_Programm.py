6a82889a287433dc1ad3559eff68458e0e1f2bbe
Maryam Salarhaji <93576036+MaryamSalarhaji@users.noreply.github.com>
2023-09-10 13:33:29 +0330

    Update Python_Basics_Programm.py

--- Python_Basics_Programm.py
+++ Python_Basics_Programm.py
@@ -1,4 +1,4 @@
-#Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+#This function accepts two integer numbers return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
 def multiplication_or_sum(number_1, number_2):
     product=number_1*number_2
     sum=number_1+number_2
